f39a2c3b6d016f4cb3d8abf5ba37798668cb4a93
Shucheng Yan <shucheng.yan0619@gmail.com>
2019-11-16 17:22:53 -0500

    fix+test

---
+++ code/AD.py
@@ -0,0 +1,275 @@
+import numpy as np
+
+
+class AD:
+    """
+
+    """
+
+    def __init__(self):
+        self.val = 0
+        self.der = 0
+
+    def __add__(self, other):
+        """
+
+        Parameters
+        ----------
+        other
+
+        Returns
+        -------
+
+        """
+        newAutoDiff = AD()
+
+        if isinstance(other, AD):
+            newAutoDiff.val = self.val + other.val
+            newAutoDiff.der = self.der + other.der
+
+        elif isinstance(other, (int, float)):
+            newAutoDiff.val = self.val + other
+            newAutoDiff.der = self.der
+
+        else:
+            raise ValueError('operand in addition is invalid')
+        return newAutoDiff
+
+    def __radd__(self, other):
+        """
+
+        Parameters
+        ----------
+        other
+
+        Returns
+        -------
+
+        """
+        return self.__add__(other)
+
+    def __sub__(self, other):
+        """
+
+        Parameters
+        ----------
+        other
+
+        Returns
+        -------
+
+        """
+        newAutoDiff = AD()
+        if isinstance(other, AD):
+            newAutoDiff.val = self.val - other.val
+            newAutoDiff.der = self.der - other.der
+
+        elif isinstance(other, (int, float)):
+            newAutoDiff.val = self.val - other
+            newAutoDiff.der = self.der
+        else:
+            raise ValueError('operand in subtraction is invalid')
+        return newAutoDiff
+
+    def __rsub__(self, other):
+        """
+
+        Parameters
+        ----------
+        other
+
+        Returns
+        -------
+
+        """
+        newAutoDiff = AD()
+        if isinstance(other, (int, float)):
+            newAutoDiff.val = other - self.val
+            newAutoDiff.der = self.der * -1
+
+        else:
+            raise ValueError('operand in subtraction is invalid')
+        return newAutoDiff
+
+    def __mul__(self, other):
+        """
+
+        Parameters
+        ----------
+        other
+
+        Returns
+        -------
+
+        """
+        newAutoDiff = AD()
+
+        if isinstance(other, AD):
+            newAutoDiff.val = self.val * other.val
+            newAutoDiff.der = self.val * other.der + self.der * other.val
+        elif isinstance(other, (int, float)):
+            newAutoDiff.val = self.val * other
+            newAutoDiff.der = self.der * other
+        else:
+            raise ValueError('operand in multiplication is invalid')
+        return newAutoDiff
+
+    def __rmul__(self, other):
+        """
+
+        Parameters
+        ----------
+        other
+
+        Returns
+        -------
+
+        """
+        return self.__mul__(other)
+
+    def __div__(self, other):
+        """
+
+        Parameters
+        ----------
+        other
+
+        Returns
+        -------
+
+        """
+        newAutoDiff = AD()
+        if isinstance(other, AD):
+            newAutoDiff.val = self.val / other.val
+            newAutoDiff.der = (other.val * (self.der) + self.val * (other.der)) / other.val ** 2
+
+        elif isinstance(other, (int, float)):
+            newAutoDiff.val = self.val / other
+            newAutoDiff.der = self.der / other
+
+        else:
+            raise ValueError('operand in division is invalid')
+        return newAutoDiff
+
+    def __rdiv__(self, other):
+        """
+
+        Parameters
+        ----------
+        other
+
+        Returns
+        -------
+
+        """
+        if isinstance(other, (int, float)):
+            newAutoDiff = other * self ** -1
+        else:
+            raise ValueError('operand in division is invalid')
+        return newAutoDiff
+
+    def __pow__(self, num):
+        """
+
+        Parameters
+        ----------
+        num
+
+        Returns
+        -------
+
+        """
+        if isinstance(num, int):
+            newAutoDiff = AD()
+            newAutoDiff.val = self.val ** num
+            newAutoDiff.der = num * self.val ** (num - 1)
+        else:
+            raise AttributeError('exponential has to be an integer')
+        return newAutoDiff
+
+    def __rpow__(self, other):
+        """
+
+        Parameters
+        ----------
+        other
+
+        Returns
+        -------
+
+        """
+        newAutoDiff = AD()
+
+        newAutoDiff.val = other ** self.val
+        newAutoDiff.der = other ** self.val * np.log(other)
+        return newAutoDiff
+
+    def __str__(self):
+        """
+
+        Returns
+        -------
+
+        """
+        return 'val = {}, der={}'.format(self.val, self.der)
+
+
+class X(AD):
+    """
+
+    """
+
+    def __init__(self, alpha):
+        # Initialize as f(x) = x
+        self.val = alpha
+        self.der = 1.0
+
+
+class Sin(AD):
+    """
+
+    """
+
+    def __init__(self, fun):
+        self.val = np.sin(fun.val)
+        self.der = fun.der * np.cos(fun.val)
+
+
+class Cos(AD):
+    """
+
+    """
+
+    def __init__(self, fun):
+        self.val = np.cos(fun.val)
+        self.der = fun.der * -np.sin(fun.val)
+
+
+class Tan(AD):
+    """
+
+    """
+
+    def __init__(self, fun):
+        self.val = np.tan(fun.val)
+        self.der = fun.der / (np.cos(fun.val) ** 2)
+
+
+class Log(AD):
+    """
+
+    """
+
+    def __init__(self, fun):
+        self.val = np.log(fun.val)
+        self.der = fun.der / fun.val
+
+
+class Exp(AD):
+    """
+
+    """
+
+    def __init__(self, fun):
+        self.val = np.exp(fun.val)
+        self.der = fun.der * np.exp(fun.val)
---
+++ code/tests/test_AD.py
@@ -0,0 +1,166 @@
+import pytest
+import numpy as np
+
+from AD import X, Cos, Sin, Tan, Log, Exp
+
+
+def test_create_x():
+    x = X(2)
+    assert x.val == 2
+    assert x.der == 1
+
+
+def test_create_cos():
+    x = X(2)
+    x1 = Cos(x)
+    assert x1.val == np.cos(2)
+    assert x1.der == -np.sin(2)
+
+
+def test_create_sin():
+    x = X(2)
+    x1 = Sin(x)
+    assert x1.val == np.sin(2)
+    assert x1.der == np.cos(2)
+
+
+def test_create_tan():
+    x = X(2)
+    x1 = Tan(x)
+    assert x1.val == np.tan(2)
+    assert x1.der == 1 / np.cos(2) ** 2
+
+
+def test_create_log():
+    x = X(2)
+    x1 = Log(x)
+    assert x1.val == np.log(2)
+    assert x1.der == 0.5
+
+
+def test_create_exp():
+    x = X(2)
+    x1 = Exp(x)
+    assert x1.val == np.exp(2)
+    assert x1.der == np.exp(2)
+
+
+def test_add():
+    x = X(2)
+    f = x + 3
+
+    assert f.val == 5
+    assert f.der == 1
+
+
+def test_add1():
+    x = X(2)
+    f = x + x
+    assert f.val == 4
+    assert f.der == 2
+
+
+def test_radd():
+    x = X(2)
+    f = 3 + x
+    assert f.val == 5
+    assert f.der == 1
+
+
+def test_sub():
+    x = X(2)
+    f = x - 3
+    assert f.val == -1
+    assert f.der == 1
+
+
+def test_sub1():
+    x = X(2)
+    f = x - x
+    assert f.val == 0
+    assert f.der == 0
+
+
+def test_rsub():
+    x = X(2)
+    f = 3 - x
+    assert f.val == 1
+    assert f.der == -1
+
+
+def test_mul():
+    x = X(2)
+    f = x * 3
+    assert f.val == 6
+    assert f.der == 3
+
+
+def test_mul1():
+    x = X(2)
+    f = x * x
+    assert f.val == 4
+    assert f.der == 4
+
+
+def test_rmul():
+    x = X(2)
+    f = 3 * x
+    assert f.val == 6
+    assert f.der == 3
+
+
+def test_div():
+    x = X(2)
+    f = x / 2
+    assert f.val == 1
+    assert f.der == 0.5
+
+
+def test_div1():
+    x = X(2)
+    f = x / x
+    assert f.val == 1
+    assert f.der == 1
+
+
+def test_rdiv():
+    x = X(2)
+    f = 2 / x
+    assert f.val == 1
+    assert f.der == -0.5
+
+
+def test_pow():
+    x = X(2)
+    f = x ** 3
+    assert f.val == 8
+    assert f.der == 12
+
+
+def test_rpow():
+    x = X(2)
+    f = 3 ** x
+    assert f.val == 9
+    assert f.der == 9 * np.log(3)
+
+
+def add_invalid():
+    x = X(2)
+    f = x + 'hello'
+
+
+def test_invalid_add():
+    with pytest.raises(ValueError) as excinfo:
+        add_invalid()
+    assert 'operand in addition is invalid' == str(excinfo.value)
+
+
+def mul_invalid():
+    x = X(2)
+    f = x * 'hello'
+
+
+def test_invalid_mul():
+    with pytest.raises(ValueError) as excinfo:
+        mul_invalid()
+    assert 'operand in multiplication is invalid' == str(excinfo.value)
