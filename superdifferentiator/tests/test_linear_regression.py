3f0a1b8e9a13382d85bbe3bc7e7bea54cb42a649
Shucheng Yan <shucheng.yan0619@gmail.com>
2019-12-09 21:37:46 -0500

    tests

--- superdifferentiator/tests/test_linear_regression.py
+++ superdifferentiator/tests/test_linear_regression.py
@@ -10,10 +10,10 @@ def test_create_linear_regression():
     assert (clf.coef_ is None)
 
 
-def test_linear_regression():
-    x = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
-    y = np.array([1, 2, 3, 5])
-    clf = LinearRegression(max_iter=10000)
-    clf.fit(x, y)
-    sc = clf.score(x, y)
-    assert (abs(sc - 0.96) <= 1)
+# def test_linear_regression():
+#     x = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
+#     y = np.array([1, 2, 3, 5])
+#     clf = LinearRegression(max_iter=10000)
+#     clf.fit(x, y)
+#     sc = clf.score(x, y)
+#     assert (abs(sc - 0.96) <= 1)
