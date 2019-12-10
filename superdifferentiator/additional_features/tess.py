9948dc0f0422b3c3b1ceb2c6a2f52fc6e653d67e
Shucheng Yan <shucheng.yan0619@gmail.com>
2019-12-09 21:33:12 -0500

    fix

--- superdifferentiator/additional_features/tess.py
+++
@@ -1,9 +0,0 @@
-from superdifferentiator.forward.functions import X
-from superdifferentiator.additional_features.gradient_descent import GD
-import numpy as np
-
-
-x = X(4)
-
-fx = (3 - x)*(3-x)
-print(fx)
